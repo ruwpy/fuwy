0b7406bed8d6996308816b1f5d5183360e3983d7
ruwpy <ruwpy.who@gmail.com>
2023-06-07 21:10:43 +0200

    create container component for pages

---
+++ src/components/container.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+const Container = ({ children }: { children?: ReactNode }) => {
+  return (
+    <main className="container h-[100dvh] max-w-[1680px] mx-auto px-6 py-[20px] overflow-x-hidden pt-[100px]">
+      {children}
+    </main>
+  );
+};
+
+export default Container;
